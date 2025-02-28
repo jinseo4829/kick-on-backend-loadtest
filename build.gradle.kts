843afa2c0867fb4d03053352978954dcea213397
Minseo Lim <limms1217@naver.com>
2025-02-28 13:08:16 +0900

    compile 환경 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	// http Client
-	compileOnly("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64")
 
 
