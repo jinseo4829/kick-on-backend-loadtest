0bca4235a16fbf96be5bdf7d36bc54887105a03a
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2025-05-16 13:54:25 +0900

    컨트롤러 서비스 수정

--- src/main/java/kr/kickon/api/domain/board/BoardController.java
+++ src/main/java/kr/kickon/api/domain/board/BoardController.java
@@ -47,7 +47,6 @@ public class BoardController {
     private final JwtTokenProvider jwtTokenProvider;
     private final TeamService teamService;
     private final UUIDGenerator uuidGenerator;
-    private final UserService userService;
 
     @Operation(summary = "홈화면 함께 볼만한 게시글 리스트 조회", description = "응원팀 여부에 상관없이 최신 게시글 기준으로 10개 리스트 반환")
     @ApiResponses({
@@ -130,8 +129,7 @@ public class BoardController {
     })
     @DeleteMapping("/{boardPk}")
     public ResponseEntity<ResponseDTO> deleteBoard(@PathVariable Long boardPk){
-        //User user = jwtTokenProvider.getUserFromSecurityContext();
-        User user = userService.findById("9ba08d10-8d66-4b66-937c-8233ef7e3036");
+        User user = jwtTokenProvider.getUserFromSecurityContext();
         Board board = boardService.findByPk(boardPk);
         if(board==null) throw new NotFoundException(ResponseCode.NOT_FOUND_BOARD);
         if(board.getUser()!=user) throw new ForbiddenException(ResponseCode.FORBIDDEN);
