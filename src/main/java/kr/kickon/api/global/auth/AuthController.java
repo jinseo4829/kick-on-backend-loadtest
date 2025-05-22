42a4ebd7a3030b4bda3e212a111d85c85256ca92
Minseo Lim <limms1217@naver.com>
2025-05-22 19:31:39 +0900

    feat: admin refresh token 발급 api

--- src/main/java/kr/kickon/api/global/auth/AuthController.java
+++ src/main/java/kr/kickon/api/global/auth/AuthController.java
@@ -2,11 +2,14 @@ package kr.kickon.api.global.auth;
 
 import io.jsonwebtoken.Claims;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.kickon.api.admin.root.AdminService;
 import kr.kickon.api.domain.user.UserService;
+import kr.kickon.api.global.auth.jwt.admin.AdminJwtTokenProvider;
 import kr.kickon.api.global.auth.jwt.user.JwtTokenProvider;
 import kr.kickon.api.global.auth.jwt.dto.TokenDto;
 import kr.kickon.api.global.auth.jwt.dto.TokenRequestDTO;
 import kr.kickon.api.global.common.ResponseDTO;
+import kr.kickon.api.global.common.entities.Admin;
 import kr.kickon.api.global.common.entities.User;
 import kr.kickon.api.global.common.enums.ResponseCode;
 import kr.kickon.api.global.error.exceptions.UnauthorizedException;
@@ -28,6 +31,8 @@ import java.util.Map;
 public class AuthController {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserService userService;
+    private final AdminService adminService;
+    private final AdminJwtTokenProvider adminJwtTokenProvider;
 
     @PostMapping("/refresh")
     public ResponseEntity<ResponseDTO<TokenDto>> refreshToken(@RequestBody TokenRequestDTO request) {
@@ -47,6 +52,30 @@ public class AuthController {
         newClaims.put(jwtTokenProvider.AUTH_PK, user.getPk());
         newClaims.put(jwtTokenProvider.AUTHORITIES_KEY, authorities);
         String accessToken = jwtTokenProvider.createAccessToken(claims, user.getPk(), authorities);
+
+        return ResponseEntity.ok(ResponseDTO.success(ResponseCode.SUCCESS,
+                new TokenDto(accessToken, refreshToken)));
+    }
+
+    @PostMapping("/admin/refresh")
+    public ResponseEntity<ResponseDTO<TokenDto>> refreshAdminToken(@RequestBody TokenRequestDTO request) {
+        String refreshToken = request.getRefreshToken();
+        if (!jwtTokenProvider.validateToken(refreshToken)) {
+            throw new UnauthorizedException(ResponseCode.INVALID_REFRESH_TOKEN);
+        }
+
+        Claims claims = jwtTokenProvider.getClaimsFromToken(refreshToken);
+        Admin admin = adminService.findByPk(Long.parseLong(claims.get(jwtTokenProvider.AUTH_PK).toString()));
+        if (admin == null) throw new UnauthorizedException(ResponseCode.INVALID_REFRESH_TOKEN);
+
+        String authorities = "ADMIN";  // 관리자 권한만 부여
+
+        Map<String, Object> newClaims = new HashMap<>();
+        newClaims.put(adminJwtTokenProvider.AUTH_PK, admin.getPk());
+        newClaims.put(adminJwtTokenProvider.AUTHORITIES_KEY, authorities);
+
+        // 관리자용 access token 생성
+        String accessToken = adminJwtTokenProvider.createAccessToken(newClaims, admin.getPk(), authorities);
 
         return ResponseEntity.ok(ResponseDTO.success(ResponseCode.SUCCESS,
                 new TokenDto(accessToken, refreshToken)));
