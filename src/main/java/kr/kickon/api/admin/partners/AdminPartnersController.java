4e06e13debb8a09a24266532070e23405dd0b12a
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2025-06-25 03:41:38 +0900

    파트너스 상세 조회 기능

--- src/main/java/kr/kickon/api/admin/partners/AdminPartnersController.java
+++ src/main/java/kr/kickon/api/admin/partners/AdminPartnersController.java
@@ -8,12 +8,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
+import kr.kickon.api.admin.partners.dto.PartnersDetailDTO;
 import kr.kickon.api.admin.partners.dto.PartnersListDTO;
 import kr.kickon.api.admin.partners.request.PartnersFilterRequest;
+import kr.kickon.api.admin.partners.response.GetPartnersDetailResponse;
 import kr.kickon.api.admin.partners.response.GetPartnersResponse;
 import kr.kickon.api.global.common.PagedMetaDTO;
 import kr.kickon.api.global.common.ResponseDTO;
+import kr.kickon.api.global.common.entities.Partners;
+import kr.kickon.api.global.common.entities.User;
 import kr.kickon.api.global.common.enums.ResponseCode;
+import kr.kickon.api.global.error.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -21,6 +26,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,19 +61,17 @@ public class AdminPartnersController {
     );
   }
 
-  /*
-  @GetMapping("/{userPk}")
-  @Operation(summary = "유저 상세 조회", description = "유저의 상세 정보를 조회합니다.")
+  @GetMapping("/{partnersPk}")
+  @Operation(summary = "파트너스 상세 조회", description = "파트너스의 상세 정보를 조회합니다.")
   @ApiResponses({
       @ApiResponse(responseCode = "200", description = "성공",
-          content = @Content(schema = @Schema(implementation = GetUserDetailResponse.class))),
+          content = @Content(schema = @Schema(implementation = GetPartnersDetailResponse.class))),
   })
-  public ResponseEntity<ResponseDTO<UserDetailDTO>> getUserDetail(@PathVariable Long userPk) {
-    User user = userService.findByPk(userPk);
-    if (user == null) throw new NotFoundException(ResponseCode.NOT_FOUND_USER);
+  public ResponseEntity<ResponseDTO<PartnersDetailDTO>> getPartnersDetail(@PathVariable Long partnersPk) {
+    Partners partners = adminpartnersService.findByPk(partnersPk);
+    if (partners == null) throw new NotFoundException(ResponseCode.NOT_FOUND_USER);
+    PartnersDetailDTO dto = adminpartnersService.getPartnersDetail(partners);
 
-    List<UserFavoriteTeam> favoriteTeams = userFavoriteTeamService.findAllByUserPk(userPk);
-
-    return ResponseEntity.ok(ResponseDTO.success(ResponseCode.SUCCESS, new UserDetailDTO(user, favoriteTeams)));
-  }*/
+    return ResponseEntity.ok(ResponseDTO.success(ResponseCode.SUCCESS, dto));
+  }
 }
---
+++ src/main/java/kr/kickon/api/admin/partners/response/GetPartnersDetailResponse.java
@@ -0,0 +1,8 @@
+package kr.kickon.api.admin.partners.response;
+
+import kr.kickon.api.admin.partners.dto.PartnersDetailDTO;
+import kr.kickon.api.global.common.ResponseDTO;
+
+public class GetPartnersDetailResponse extends ResponseDTO<PartnersDetailDTO> {
+
+}
