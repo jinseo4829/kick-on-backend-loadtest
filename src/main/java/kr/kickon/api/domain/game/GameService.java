0be3d17b60dc884262727b399998b91bd7e000bb
Minseo Lim <limms1217@naver.com>
2025-08-03 21:28:18 +0900

    feat: 게임 결과 migration 시간 조정

--- src/main/java/kr/kickon/api/domain/game/GameService.java
+++ src/main/java/kr/kickon/api/domain/game/GameService.java
@@ -219,7 +219,7 @@ public class GameService implements BaseService<Game> {
 
         // 현재 시간과 4시간 전 시간 계산
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime past24Hours = now.minusHours(2400);
+        LocalDateTime past24Hours = now.minusHours(4320);
 
         // QueryDSL을 사용하여 24시간 이내에 시작한 게임 중 종료된 게임을 조회
         return queryFactory
