5e43bd225d9e8c140f9f071c554bd9c20269f48c
가은정 <127952295+khmandarrin@users.noreply.github.com>
2025-08-08 00:41:52 +0900

    Revert "Revert "Ko 514: emdeddedLink 엔티티 추가""

---
+++ src/main/java/kr/kickon/api/domain/embeddedLink/EmbeddedLinkService.java
@@ -0,0 +1,30 @@
+package kr.kickon.api.domain.embeddedLink;
+
+import java.util.List;
+import kr.kickon.api.global.common.entities.EmbeddedLink;
+import kr.kickon.api.global.common.enums.UsedInType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EmbeddedLinkService {
+
+  private final EmbeddedLinkRepository embeddedLinkRepository;
+  public void saveAll(List<EmbeddedLink> links) {
+    embeddedLinkRepository.saveAll(links);
+  }
+
+  public List<EmbeddedLink> findByBoardPk(Long boardPk){
+    return embeddedLinkRepository.findByUsedInEqualsAndReferencePkEquals(UsedInType.BOARD, boardPk);
+  }
+
+  public List<EmbeddedLink> findByNewsPk(Long newsPk){
+    return embeddedLinkRepository.findByUsedInEqualsAndReferencePkEquals(UsedInType.NEWS, newsPk);
+  }
+  public void save(EmbeddedLink embeddedLink) {
+    embeddedLinkRepository.save(embeddedLink);
+  }
+}
