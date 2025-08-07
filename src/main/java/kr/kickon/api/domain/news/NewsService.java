f0e00d95e26d0199842942565543e15f74385e19
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2025-08-08 00:32:38 +0900

    수정

--- src/main/java/kr/kickon/api/domain/news/NewsService.java
+++ src/main/java/kr/kickon/api/domain/news/NewsService.java
@@ -64,7 +64,7 @@ public class NewsService implements BaseService<News> {
     }
     // endregion
 
-    // region {createNewsWithImages} 뉴스 생성 + 이미지 사용 처리
+    // region {createNewsWithImages} 뉴스 생성 + 이미지/영상 사용 처리
     @Transactional
     public News createNewsWithMedia(News news, String[] usedImageKeys, String[] usedVideoKeys, String[] embeddedLinks) {
         News savedNewsEntity = newsRepository.save(news);
